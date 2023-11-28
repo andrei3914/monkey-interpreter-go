2d3233173665c5757d2163b2443f9d64538e7a0b
Andrei P <popandrei230@yahoo.com>
2023-11-28 22:06:06 +0200

    Add support for string concatenation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -225,6 +225,10 @@ func TestErrorHandling(t *testing.T) {
             `,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -309,6 +313,18 @@ func TestClosures(t *testing.T) {
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
 	if !ok {
