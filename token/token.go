9f984e19129840c8a7a97448d7fa55b44862f464
ndrpp <popandrei230@yahoo.com>
2023-12-02 21:55:30 +0200

    Add support for macro binding to names

--- token/token.go
+++ token/token.go
@@ -50,6 +50,7 @@ const (
 	RETURN   = "RETURN"
 	TRUE     = "TRUE"
 	FALSE    = "FALSE"
+	MACRO    = "MACRO"
 )
 
 var keywords = map[string]TokenType{
@@ -60,6 +61,7 @@ var keywords = map[string]TokenType{
 	"return": RETURN,
 	"true":   TRUE,
 	"false":  FALSE,
+	"macro":  MACRO,
 }
 
 func LookupIdent(ident string) TokenType {
