34c942359d300e8b169369495f1c53ed1dfd529b
ndrpp <popandrei230@yahoo.com>
2023-12-30 16:54:30 +0200

    Compile local bindings

--- code/code.go
+++ code/code.go
@@ -33,6 +33,8 @@ const (
 
 	OpSetGlobal
 	OpGetGlobal
+	OpSetLocal
+	OpGetLocal
 
 	OpArray
 	OpHash
@@ -68,6 +70,8 @@ var definitions = map[Opcode]*Definition{
 	OpCall:          {"OpCall", []int{}},
 	OpReturnValue:   {"OpReturnValue", []int{}},
 	OpReturn:        {"OpReturn", []int{}},
+	OpSetLocal:      {"OpSetLocal", []int{1}},
+	OpGetLocal:      {"OpGetLocal", []int{1}},
 }
 
 type Definition struct {
@@ -97,6 +101,8 @@ func Make(op Opcode, operands ...int) []byte {
 	for i, o := range operands {
 		width := def.OperandWidths[i]
 		switch width {
+		case 1:
+			instruction[offset] = byte(o)
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
 		}
@@ -112,6 +118,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 	for i, width := range def.OperandWidths {
 		switch width {
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
 		}
@@ -124,6 +132,10 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 func ReadUint16(ins Instructions) uint16 {
 	return binary.BigEndian.Uint16(ins)
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
 
 func Lookup(op byte) (*Definition, error) {
--- vm/vm.go
+++ vm/vm.go
@@ -194,33 +194,33 @@ func (vm *VM) Run() error {
 				return err
 			}
 
-        case code.OpCall:
-            fn, ok := vm.stack[vm.sp-1].(*object.CompiledFunction)
-            if !ok {
-                return fmt.Errorf("calling non function")
-            }
-            frame := NewFrame(fn)
-            vm.pushFrame(frame)
+		case code.OpCall:
+			fn, ok := vm.stack[vm.sp-1].(*object.CompiledFunction)
+			if !ok {
+				return fmt.Errorf("calling non function")
+			}
+			frame := NewFrame(fn)
+			vm.pushFrame(frame)
 
-        case code.OpReturnValue:
-            returnValue := vm.pop()
+		case code.OpReturnValue:
+			returnValue := vm.pop()
 
-            vm.popFrame()
-            vm.pop()
+			vm.popFrame()
+			vm.pop()
 
-            err := vm.push(returnValue)
-            if err != nil {
-                return err
-            }
+			err := vm.push(returnValue)
+			if err != nil {
+				return err
+			}
 
-        case code.OpReturn:
-            vm.popFrame()
-            vm.pop()
+		case code.OpReturn:
+			vm.popFrame()
+			vm.pop()
 
-            err := vm.push(Null)
-            if err != nil {
-                return err
-            }
+			err := vm.push(Null)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
